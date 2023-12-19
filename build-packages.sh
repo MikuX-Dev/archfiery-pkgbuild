24c57d23be1a149966d0add384371d981fb871bb
MikuX-Dev <abinishanth3464@gmail.com>
2023-12-19 10:16:31 +0530

    done

--- build-packages.sh
+++ build-packages.sh
@@ -9,21 +9,41 @@
 # get hidden bugs that are hard to discover.
 set -euo pipefail
 
+# for dir in x86_64/*/; do
+#   cd "$dir"
+#   NAME=${dir%*/}
+#   curl -sSL "https://aur.archlinux.org/cgit/aur.git/plain/PKGBUILD?h=$NAME" -o ./PKGBUILD
+#   if [ -f PKGBUILD ]; then
+#     if cmp --silent -- "PKGBUILD" "./PKGBUILD"; then
+#       echo "## Checking the PKGBUILD: PKGBUILD for $NAME has not changed."
+#       makepkg -sf --noconfirm --needed --noprogressbar
+#     else
+#       echo "++ Checking the PKGBUILD: PKGBUILD for $NAME has changed."
+#       mv "./PKGBUILD" PKGBUILD
+#       makepkg -sf --noconfirm --needed --noprogressbar || echo "FAILED TO MAKE PACKAGE: $NAME"
+#     fi
+#   else
+#     echo "@@ PKGBUILD not available: $NAME is not in the AUR. Skipping!"
+#   fi
+#   cd -
+# done
+
+output_dir="output"
+
 for dir in x86_64/*/; do
   cd "$dir"
   NAME=${dir%*/}
-  curl -sSL "https://aur.archlinux.org/cgit/aur.git/plain/PKGBUILD?h=$NAME" -o ./PKGBUILD
-  if [ -f PKGBUILD ]; then
-    if cmp --silent -- "PKGBUILD" "./PKGBUILD"; then
-      echo "## Checking the PKGBUILD: PKGBUILD for $NAME has not changed."
-      makepkg -sf --noconfirm --needed --noprogressbar
-    else
-      echo "++ Checking the PKGBUILD: PKGBUILD for $NAME has changed."
-      mv "./PKGBUILD" PKGBUILD
-      makepkg -sf --noconfirm --needed --noprogressbar || echo "FAILED TO MAKE PACKAGE: $NAME"
-    fi
-  else
-    echo "@@ PKGBUILD not available: $NAME is not in the AUR. Skipping!"
+  if [[ ! $NAME =~ ^[a-z0-9_-]+$ ]]; then
+    echo "Invalid name: $NAME" >&2
+    continue
   fi
+
+  if ! makepkg -sf --noconfirm --needed --noprogressbar; then
+    echo "Failed to build $NAME" >&2
+    continue
+  fi
+
+  cp -r *.tar.* "$output_dir/"
   cd -
 done
+echo "Done building packages"
