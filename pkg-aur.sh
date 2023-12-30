3e7839ed15fba91b942c2b7efa27b3fc330d8c15
MikuX-Dev <abinishanth3464@gmail.com>
2023-12-30 22:10:36 +0530

    gpt

--- pkg-aur.sh
+++ pkg-aur.sh
@@ -12,24 +12,23 @@ set -euo pipefail
 # Define the package names
 NAME=("btrfs-assistant" "snapper-gui-git" "mkinitcpio-firmware" "firmware-manager" "pikaur" "yay-bin")
 
+# Set the workspace directory
+WORKSPACE_DIR="$GITHUB_WORKSPACE"
+
 # Create a folder for each package and download PKGBUILD
 for pkg in "${NAME[@]}"; do
-  mkdir -p "$pkg"
-  sudo chown -R builder:builder "$pkg"
-  git clone https://aur.archlinux.org/"$pkg".git "$pkg"
+  git clone https://aur.archlinux.org/"$pkg".git "$WORKSPACE_DIR/$pkg"
 done
 
-# fix directory permissions
-sudo chown -R builder:builder "$pkg"
-sudo chown -R builder:builder output/
+# Create the "output" directory
+mkdir -p "$WORKSPACE_DIR/output"
 
 # Build and package each package
 for pkg in "${NAME[@]}"; do
-  cd "$pkg" || exit 1
+  cd "$WORKSPACE_DIR/$pkg" || exit 1
   makepkg -csf --noconfirm --needed --noprogressbar
-  cp ./*.pkg.tar.* ../output/ # Assuming output is a folder in the same directory as this script
-  cd ..
+  cp ./*.pkg.tar.* "$WORKSPACE_DIR/output/" # Assuming output is a folder in the same directory as this script
 done
 
-echo "Packages built and copied to output folder."
+echo "Packages built and copied to the output folder."
 echo "Done building packages"
