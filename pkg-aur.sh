9406bcb82b8ab4587d2c58b70cc1ae279ab70cdd
MikuX-Dev <abinishanth3464@gmail.com>
2023-12-30 22:01:12 +0530

    pkg-aur only now

--- pkg-aur.sh
+++ pkg-aur.sh
@@ -9,16 +9,17 @@
 # get hidden bugs that are hard to discover.
 set -euo pipefail
 
-sudo chown -R builder:builder .*
-
 # Define the package names
-NAME=("snapper-gui-git" "mkinitcpio-firmware" "firmware-manager" "pikaur" "yay-bin")
+NAME=("btrfs-assistant" "snapper-gui-git" "mkinitcpio-firmware" "firmware-manager" "pikaur" "yay-bin")
 
 # Create a folder for each package and download PKGBUILD
 for pkg in "${NAME[@]}"; do
-  mkdir -p "$pkg"
-  curl -sSL "https://aur.archlinux.org/cgit/aur.git/plain/PKGBUILD?h=$pkg" -o "$pkg/PKGBUILD"
+  git clone https://aur.archlinux.org/"$pkg".git
 done
+
+# fix directory permissions
+sudo chown -R builder:builder "$pkg"
+sudo chown -R builder:builder output/
 
 # Build and package each package
 for pkg in "${NAME[@]}"; do
