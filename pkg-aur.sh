4639a8b65fd90a58e2c083919e67b7771f06254a
MikuX-Dev <abinishanth3464@gmail.com>
2023-12-30 22:34:05 +0530

    done

--- pkg-aur.sh
+++ pkg-aur.sh
@@ -32,3 +32,40 @@ done
 
 echo "Packages built and copied to the output folder."
 echo "Done building packages"
+
+# this is my old
+# #!/usr/bin/env bash
+
+# # Script name: build-packages.sh
+# # Description: Script for automating 'makepkg' on the PKGBUILDS.
+# # Contributors: MikuX-Dev
+
+# # Set with the flags "-e", "-u","-o pipefail" cause the script to fail
+# # if certain things happen, which is a good thing.  Otherwise, we can
+# # get hidden bugs that are hard to discover.
+# set -euo pipefail
+
+# # Define the package names
+# NAME=("btrfs-assistant" "snapper-gui-git" "mkinitcpio-firmware" "firmware-manager" "pikaur" "yay-bin")
+
+# # Create a folder for each package and download PKGBUILD
+# for pkg in "${NAME[@]}"; do
+#   mkdir -p "$pkg"
+#   sudo chown -R builder:builder "$pkg"
+#   git clone https://aur.archlinux.org/"$pkg".git "$pkg"
+# done
+
+# # fix directory permissions
+# sudo chown -R builder:builder "$pkg"
+# sudo chown -R builder:builder output/
+
+# # Build and package each package
+# for pkg in "${NAME[@]}"; do
+#   cd "$pkg" || exit 1
+#   makepkg -csf --noconfirm --needed --noprogressbar
+#   cp ./*.pkg.tar.* ../output/ # Assuming output is a folder in the same directory as this script
+#   cd ..
+# done
+
+# echo "Packages built and copied to output folder."
+# echo "Done building packages"
