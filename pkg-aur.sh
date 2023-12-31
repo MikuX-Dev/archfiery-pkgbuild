3b26c358c3ac5c3384625b9cebad32e5a4874589
MikuX-Dev <abinishanth3464@gmail.com>
2023-12-31 22:45:38 +0530

    done

--- pkg-aur.sh
+++ pkg-aur.sh
@@ -14,19 +14,14 @@ if [ -d "/github" ]; then
   sudo chown -R builder:builder /github/workspace /github/home
 fi
 
-WORKSPACE_DIR=${GITHUB_WORKSPACE:-$(pwd)}
-
-# Create the "output" directory
-mkdir "$WORKSPACE_DIR/output"
-
 packages=("btrfs-assistant" "snapper-gui-git" "mkinitcpio-firmware" "firmware-manager" "pikaur" "yay-bin") # Add your package names here
 
 # Loop through the packages, clone, build, and copy
 for package in "${packages[@]}"; do
-  git clone "https://aur.archlinux.org/$package.git"
+  git clone https://aur.archlinux.org/"$package".git
   cd "$package" || exit
   makepkg -csf --noconfirm --needed --noprogressbar
-  cp -r ./*.pkg.* "$WORKSPACE_DIR"/output # Replace /path/to/output/directory with the actual path
+  cp -r ./*.pkg.* ../output # Replace /path/to/output/directory with the actual path
   cd ..
 done
 
