7cbdd794c6e2452f1f5ea35baf2ed29f5ec99792
MikuX-Dev <abinishanth3464@gmail.com>
2024-01-01 21:18:06 +0530

    done

---
+++ push-aur.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+# shellcheck disable=SC2035
+
+# Script name: build-packages.sh
+# Description: Script for automating 'makepkg' on the PKGBUILDS.
+# Contributors: MikuX-Dev
+
+# Set with the flags "-e", "-u","-o pipefail" cause the script to fail
+# if certain things happen, which is a good thing.  Otherwise, we can
+# get hidden bugs that are hard to discover.
+set -euo pipefail
+
+git clone git@github.com:MikuX-Dev/archfiery-repo.git /home/builder/archfiery-repo
+cd ../archfiery-repo/
+git lfs install
+
+cd x86_64/
+rm -rf ./*.pkg.tar.*
+
+cp -r /home/builder/output/large/* ./
+
+cat >>.gitattributes <<EOF
+*.zst filter=lfs diff=lfs merge=lfs -text
+*.zst !text !filter !merge !diff
+EOF
+
+# Track large packages with Git LFS and echo each name
+for package in /home/builder/output/large/*.pkg.tar.*; do
+  name=$(basename "$package")
+  echo "Tracking $name with Git LFS"
+  git lfs track "$name"
+done
+
+mv .gitattributes /home/builder/archfiery-repo/
+cp -r /home/builder/output/small/* ./
+
+chmod +x ./update-db.sh
+./update-db.sh
+
+cd /home/builder/archfiery-repo/
+git config --local user.email "actions@github.com"
+git config --local user.name "GitHub Actions"
+git add .
+git commit -m "Add built packages"
+git push
