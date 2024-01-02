aeb417608b997beabf402d25eb5387a883dfe08c
MikuX-Dev <abinishanth3464@gmail.com>
2024-01-02 15:52:43 +0530

    done

--- build-aur.sh
+++ build-aur.sh
@@ -33,7 +33,17 @@ set -euo pipefail
 packages=("aosp-devel" "appmenu-gtk-module-git" "appmenu-qt4" "bluez-firmware" "brave-bin" "btrfs-assistant" "caffeine-ng" "downgrade" "fancontrol-gui" "firebuild" "firmware-manager" "gtk3-nocsd-git" "lib32-ncurses5-compat-libs" "libdbusmenu-glib" "libdbusmenu-gtk2" "libdbusmenu-gtk3" "lineageos-devel" "megasync-bin" "mkinitcpio-firmware" "mkinitcpio-numlock" "mugshot" "nbfc" "ncurses5-compat-libs" "obsidian-bin" "ocs-url" "octopi" "pomatez" "repoctl" "rtl8821ce-dkms-git" "rtw89-dkms-git" "simplescreenrecorder" "snapper-gui-git" "stacer-bin" "tela-icon-theme" "thunar-extended" "thunar-megasync-bin" "thunar-secure-delete" "thunar-shares-plugin" "thunar-vcs-plugin" "tlpui" "universal-android-debloater-bin" "vala-panel-appmenu-common-git" "vala-panel-appmenu-registrar-git" "vala-panel-appmenu-xfce-git" "visual-studio-code-bin" "xfce4-docklike-plugin" "xfce4-panel-profiles" "xml2" "yay-bin" "zsh-theme-powerlevel10k-git" "pikaur")
 
 for package in "${packages[@]}"; do
-  pikaur -Sw --noconfirm --needed --keepbuilddeps --nodiff "$package"
+  # pikaur -Sw --noconfirm --needed --keepbuilddeps --nodiff "$package"
+
+  yay -S --noconfirm --needed "$package"
+
+  OUTPUT_DIR=/home/builder/output
+  YAY_DIR=/home/builder/.cache/yay
+
+  for dir in "$YAY_DIR"/*/; do
+    cp "$dir"/*.pkg.tar.* "$OUTPUT_DIR"
+  done
+
   # git clone https://aur.archlinux.org/"$package".git /tmp/pkg-build/"$package"
   # cd /tmp/pkg-build/"$package" || exit
   # makepkg -cs --noconfirm --needed --noprogressbar --skippgpcheck --skipchecksums --nosign
@@ -41,13 +51,10 @@ for package in "${packages[@]}"; do
   # cd -
 done
 
-yay -S --noconfirm --needed eww-x11
-cp -r /home/builder/.cache/yay/eww/*.pkg.tar.* /home/builder/output-small
-
 pkgsize=$(du -s *.pkg.tar.* | cut -f1)
 if [ "$pkgsize" -gt $((100 * 1024)) ]; then
   echo "$package >= 100MB"
-  cp -r /home/builder/.cache/pikaur/pkg/*.pkg.tar.* /home/builder/output-large
+  cp -r /home/builder/output/*.pkg.tar.* /home/builder/output-large
 else
-  cp -r /home/builder/.cache/pikaur/pkg/*.pkg.tar.* /home/builder/output-small
+  cp -r /home/builder/output/*.pkg.tar.* /home/builder/output-small
 fi
