f26b4c5f3182f60083a12571c2c170747254e5f7
MikuX-Dev <abinishanth3464@gmail.com>
2024-01-03 12:09:13 +0530

    done

--- build-aur.sh
+++ build-aur.sh
@@ -12,6 +12,39 @@ set -euo pipefail
 
 cd /home/builder
 
+# Function to categorize packages based on size
+categorize_packages() {
+  local input_dir="$1"
+  local large_output_dir="/home/builder/output-large"
+  local small_output_dir="/home/builder/output-small"
+
+  mkdir -p "$large_output_dir" "$small_output_dir"
+
+  for pkg_file in "$input_dir"/*.pkg.tar.*; do
+    size=$(du -m "$pkg_file" | cut -f1)
+    if [ "$size" -gt 100 ]; then
+      cp -r "$pkg_file" "$large_output_dir"
+    else
+      cp -r "$pkg_file" "$small_output_dir"
+    fi
+  done
+}
+
+#  >>>-----------------> "eww-x11" needed for archiso <--------------------<<<
+packages=("aosp-devel" "appmenu-gtk-module-git" "appmenu-qt4" "bluez-firmware" "brave-bin" "btrfs-assistant" "caffeine-ng" "downgrade" "fancontrol-gui" "firebuild" "firmware-manager" "gtk3-nocsd-git" "lib32-ncurses5-compat-libs" "libdbusmenu-glib" "libdbusmenu-gtk2" "libdbusmenu-gtk3" "lineageos-devel" "megasync-bin" "mkinitcpio-firmware" "mkinitcpio-numlock" "mugshot" "nbfc" "ncurses5-compat-libs" "obsidian-bin" "ocs-url" "octopi" "pomatez" "repoctl" "simplescreenrecorder" "snapper-gui-git" "stacer-bin" "tela-icon-theme" "thunar-extended" "thunar-megasync-bin" "thunar-secure-delete" "thunar-shares-plugin" "thunar-vcs-plugin" "tlpui" "universal-android-debloater-bin" "vala-panel-appmenu-common-git" "vala-panel-appmenu-registrar-git" "vala-panel-appmenu-xfce-git" "visual-studio-code-bin" "xfce4-docklike-plugin" "xfce4-panel-profiles" "xml2" "yay-bin" "zsh-theme-powerlevel10k-git" "eww-x11")
+
+yay -S --noconfirm --needed "${packages[@]}"
+
+OUTPUT_DIR=/home/builder/output
+YAY_DIR=/home/builder/.cache/yay/
+
+for dir in "$YAY_DIR"/*/; do
+  cp -r "$dir"/*.pkg.tar.* "$OUTPUT_DIR"
+done
+
+# Categorize packages based on size
+categorize_packages "$OUTPUT_DIR"
+
 # update_pkg() {
 #   local pkg="$1"
 #   # Remove validpgpkeys variable
@@ -31,32 +64,14 @@ cd /home/builder
 # cp -r ./*.pkg.tar.* /home/builder/output
 # cd -
 
-#  >>>-----------------> "eww-x11" needed for archiso <--------------------<<<
-packages=("aosp-devel" "appmenu-gtk-module-git" "appmenu-qt4" "bluez-firmware" "brave-bin" "btrfs-assistant" "caffeine-ng" "downgrade" "fancontrol-gui" "firebuild" "firmware-manager" "gtk3-nocsd-git" "lib32-ncurses5-compat-libs" "libdbusmenu-glib" "libdbusmenu-gtk2" "libdbusmenu-gtk3" "lineageos-devel" "megasync-bin" "mkinitcpio-firmware" "mkinitcpio-numlock" "mugshot" "nbfc" "ncurses5-compat-libs" "obsidian-bin" "ocs-url" "octopi" "pomatez" "repoctl" "rtl8821ce-dkms-git" "rtw89-dkms-git" "simplescreenrecorder" "snapper-gui-git" "stacer-bin" "tela-icon-theme" "thunar-extended" "thunar-megasync-bin" "thunar-secure-delete" "thunar-shares-plugin" "thunar-vcs-plugin" "tlpui" "universal-android-debloater-bin" "vala-panel-appmenu-common-git" "vala-panel-appmenu-registrar-git" "vala-panel-appmenu-xfce-git" "visual-studio-code-bin" "xfce4-docklike-plugin" "xfce4-panel-profiles" "xml2" "yay-bin" "zsh-theme-powerlevel10k-git" "pikaur")
+# cp -r "$PIK_DIR"/* "$OUTPUT_DIR"
 
-for package in "${packages[@]}"; do
-  # pikaur -Sw --noconfirm --needed --keepbuilddeps --nodiff "$package"
+# for package in "${packages[@]}"; do
+# pikaur -Sw --noconfirm --needed --keepbuilddeps --nodiff "$package"
 
-  yay -S --noconfirm --needed "$package"
-
-  OUTPUT_DIR=/home/builder/output
-  YAY_DIR=/home/builder/.cache/yay
-
-  for dir in "$YAY_DIR"/*/; do
-    cp "$dir"/*.pkg.tar.* "$OUTPUT_DIR"
-  done
-
-  # git clone https://aur.archlinux.org/"$package".git /tmp/pkg-build/"$package"
-  # cd /tmp/pkg-build/"$package" || exit
-  # makepkg -cs --noconfirm --needed --noprogressbar --skippgpcheck --skipchecksums --nosign
-  # cp -r *.pkg.tar.* /home/builder/output
-  # cd -
-done
-
-pkgsize=$(du -s *.pkg.tar.* | cut -f1)
-if [ "$pkgsize" -gt $((100 * 1024)) ]; then
-  echo "$package >= 100MB"
-  cp -r /home/builder/output/*.pkg.tar.* /home/builder/output-large
-else
-  cp -r /home/builder/output/*.pkg.tar.* /home/builder/output-small
-fi
+# git clone https://aur.archlinux.org/"$package".git /tmp/pkg-build/"$package"
+# cd /tmp/pkg-build/"$package" || exit
+# makepkg -cs --noconfirm --needed --noprogressbar --skippgpcheck --skipchecksums --nosign
+# cp -r *.pkg.tar.* /home/builder/output
+# cd -
+# done
